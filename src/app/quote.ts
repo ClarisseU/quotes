c87b650a6b3260e3ff86c139c3d1fa42f4b88276
ClarisseU <clarisseum@gmail.com>
2019-08-26 09:55:26 +0200

    add some changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,11 @@
 export class Quote {
     showDescription: boolean;
+    public upvote: number;
+    public downvote: number;
     constructor(public quote: string, public name: string, public author: string,public completeDate: Date){
         this.showDescription = false;
+        this.upvote = 0;
+        this.downvote = 0;
     }
     
 }
