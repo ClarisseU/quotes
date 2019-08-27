492a060a131b2042eb32b2ad49d046644157d77d
Clarisse U <clarisseum@gmail.com>
2019-08-27 10:17:54 +0200

    add some changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,23 +10,17 @@ export class QuoteComponent implements OnInit {
     new Quote('Do not cry because it is over, smile because it happened','Watch finding Nemo', 'Dr. Seuss',new Date(2019,3,14)),
     new Quote( 'The first step toward success is taken when you refuse to be a captive of the environment in which you first find yourself.','Watch finding Nemo', 'Mark Caine',new Date(2019,3,14)),
   ];
+  //function to show details
   toggleDetails(index){
     this.quotes[index].showDescription = !this.quotes[index].showDescription;
   }
+  //function to delete a quote
   completeQuote(isComplete, index){
     if(isComplete){
       this.quotes.splice(index,1)
     }
   }
-  // deleteQuote(isComplete, index){
-  //   if (isComplete) {
-  //     let toDelete = confirm(`Are you sure you want to delete ${this.quotes[index].name}?`)
-
-  //     if (toDelete){
-  //       this.quotes.splice(index,1)
-  //     }
-  //   }
-  // }
+  //function of the button that add a new quote
   addNewQuote(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
